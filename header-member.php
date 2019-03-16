dbe1cd35568893d858fc0af576ecf29158edd51d
Kevin Nguyen <minhkhoi.nguyen@solislab.com>
2019-03-16 16:30:10 +0700

    Add header variants

---
+++ header-member.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Custom fonts for this template-->
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/css/main.min.css" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class('bg-gradient-primary'); ?>>
