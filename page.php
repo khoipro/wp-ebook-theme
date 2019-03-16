6919bfb05ac2295c0fa5b35b285966ce10fd9460
Kevin Nguyen <minhkhoi.nguyen@solislab.com>
2019-03-16 16:30:33 +0700

    Add single ebook, basic page templates

---
+++ page.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template: Basic Page
+ */
+get_header();
+
+get_template_part('modules/page', 'title');
+
+get_template_part('modules/page', 'content');
+
+get_footer();
+?>
---
+++ single-ebook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template: Single Ebook Post Type
+ */
+get_header();
+
+get_template_part('modules/page', 'title');
+
+get_template_part('modules/ebook', 'content');
+
+get_template_part('modules/modal', 'report');
+
+get_footer();
+
+?>
